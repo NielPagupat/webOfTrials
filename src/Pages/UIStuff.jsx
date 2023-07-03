b292e981121b4c5b848d41933150faf532e42507
NielPagupat <niel.pagupat.2016@gmail.com>
2023-07-03 16:44:49 +0800

    read-only update

--- src/Pages/UIStuff.jsx
+++ src/Pages/UIStuff.jsx
@@ -14,7 +14,7 @@ export default function UIStuff() {
     <Box sx={{p:5, display:'flex', flexDirection:'column'}}>
         <Paper sx={{flexGrow:1, display:'flex', alignItems:'center', flexDirection:'column', p:1}}>
             <h4>Values Show Up Here</h4>
-            <TextField value={value} InputProps={{readOnly: false}}/>
+            <TextField value={value} InputProps={{readOnly: true}}/>
         </Paper>
         <Paper sx={{mt:1, bgcolor:'gray', p:1, display:'flex'}}>
             <Box sx={{bgcolor:'white', p:2, m:2}}>
